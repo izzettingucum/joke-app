99ecb79f1a45877e16237fbc4686b83b742127ed
İzzettin Gücüm <izzettin_43@hotmail.com>
2024-08-22 16:44:39 +0300

    remove welcome route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\JokeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 route::get('/joke', [JokeController::class, 'displayIndexPage']);
 route::post('/joke', [JokeController::class, 'getJoke']);
